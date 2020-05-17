7b95b1f11949d6ab9c4f6fa0a0ebd60903857a96
axiaopeng <2976755518@qq.com>
2020-05-17 13:09:43 +0800

    dist

---
+++ dist/Js/2.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[2],{
+
+/***/ "./src/viewjs/homeFollow.js":
+/*!**********************************!*\
+  !*** ./src/viewjs/homeFollow.js ***!
+  \**********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (// const leftMainTwo =\n{\n  template: \"\\n<div>\\n<div class=\\\"nofollow\\\">\\n          <i class=\\\"iconfont icon-wenben\\\"></i>\\n          <span>\\u8FD8\\u6CA1\\u6709\\u5173\\u6CE8\\u7684\\u4EBA\\uFF0C\\u4E3A\\u4F60\\u63A8\\u8350\\u4EE5\\u4E0B\\u7528\\u6237</span>\\n      </div>\\n</div>\\n\"\n});\n\n//# sourceURL=webpack:///./src/viewjs/homeFollow.js?");
+
+/***/ })
+
+}]);
